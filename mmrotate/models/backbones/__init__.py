c6f1c561e48d574617c11303d33e09fc708995f0
voodoocjl <voodoocjl@hotmail.com>
2024-07-01 14:39:08 +0800

    7/1

---
+++ mmrotate/models/backbones/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .re_resnet import ReResNet
+from .vision_transformer import VisionTransformer
+from .hivit import HiViT
+from .lsknet import LSKNet
+
+__all__ = ['ReResNet', 'VisionTransformer', 'HiViT', 'LSKNet']
