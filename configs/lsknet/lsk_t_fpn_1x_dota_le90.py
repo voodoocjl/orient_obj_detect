76f029ef2acdc64f174b624d638813c63fdf8dee
voodoocjl <voodoocjl@hotmail.com>
2024-07-15 11:18:16 +0800

    7/15

--- configs/lsknet/lsk_t_fpn_1x_dota_le90.py
+++ configs/lsknet/lsk_t_fpn_1x_dota_le90.py
@@ -18,7 +18,7 @@ model = dict(
         drop_rate=0.1,
         drop_path_rate=0.1,
         depths=[3, 3, 5, 2],
-        init_cfg=dict(type='Pretrained', checkpoint="/data/pretrained/lsk_t_backbone.pth.tar"),
+        init_cfg=dict(type='Pretrained', checkpoint="/data/pretrained/lsk_t_backbone.pth"),
         norm_cfg=dict(type='BN', requires_grad=True)),
     neck=dict(
         type='FPN',
