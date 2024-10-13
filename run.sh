e193805aed3104128b14d61655031caa506d9a35
voodoocjl <voodoocjl@hotmail.com>
2024-10-13 20:52:25 +0800

    Add files via upload

--- run.sh
+++ run.sh
@@ -12,6 +12,10 @@ cp change/distributed_mmcv.py ~/anaconda3/envs/openmmlab/lib/python3.8/site-pack
 # 复制 dist_utils.py 文件
 cp change/dist_utils.py ~/anaconda3/envs/openmmlab/lib/python3.8/site-packages/mmcv/runner/dist_utils.py
 
+cp change/base_module.py ~/anaconda3/envs/openmmlab/lib/python3.8/site-packages/mmcv/runner/base_module.py
+
+cp change/__init__.py ~/anaconda3/envs/openmmlab/lib/python3.8/site-packages/mmcv/__init__.py
+
 cp -r mmrotate/ ~/mmrotate/
 
 cp -r models/ ~/mmrotate/
